ca209f51f4ba2880e66c36513db34be241559301
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-04-29 23:38:24 -0300

    Resposta da questão 5 dos exercicios de variaveis e controle de fluxo.

---
+++ exercicios-variaveis-e-controle-de-fluxo/src/Questao05.java
@@ -0,0 +1,15 @@
+// Escreva um programa Java que converta graus Celsius para Fahrenheit.
+
+public class Questao05 {
+
+    public static void main(String[] args) {
+
+        double temperaturaEmGrausCelsius;
+        double temperaturaEmFahrenheit;
+
+        temperaturaEmGrausCelsius = 24;
+        temperaturaEmFahrenheit = (temperaturaEmGrausCelsius * 1.8) + 32;
+
+        System.out.println(temperaturaEmFahrenheit);
+    }
+}
