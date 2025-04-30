0ce3dee0f49dafd43f33df8857b1421a4c78ff50
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-04-29 23:34:18 -0300

    Resposta da questão 4 dos exercicios de variaveis e controle de fluxo.

---
+++ exercicios-variaveis-e-controle-de-fluxo/src/Questao04.java
@@ -0,0 +1,19 @@
+// Escreva um programa que determine se um número é positivo, negativo ou zero.
+
+public class Questao04 {
+
+    public static void main(String[] args) {
+
+        int numero;
+
+        numero = 10;
+
+        if (numero > 0) {
+            System.out.println("O número é positivo.");
+        } else if (numero == 0) {
+            System.out.println("O número é igual a zero.");
+        } else {
+            System.out.println("O número é negativo.");
+        }
+    }
+}
