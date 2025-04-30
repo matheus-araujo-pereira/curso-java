273918bb6a8f72645a78aef3181dbbb58e969bee
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-04-29 23:30:01 -0300

    Resposta da questão 3 dos exercicios de variaveis e controle de fluxo.

---
+++ exercicios-variaveis-e-controle-de-fluxo/src/Questao03.java
@@ -0,0 +1,20 @@
+// Escreva um programa Java que calcule a média de três números inteiros.
+
+public class Questao03 {
+
+    public static void main(String[] args) {
+
+        int primeiroNumero;
+        int segundoNumero;
+        int terceiroNumero;
+        int media;
+
+        primeiroNumero = 10;
+        segundoNumero = 5;
+        terceiroNumero = 0;
+
+        media = (primeiroNumero + segundoNumero + terceiroNumero) / 3;
+
+        System.out.println(media);
+    }
+}
