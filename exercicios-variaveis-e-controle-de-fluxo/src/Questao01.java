b788f8894e3948b58a0c06e00d79f451f8c32e9a
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-04-29 23:23:58 -0300

    Resposta da questão 1 dos exercicios de variaveis e controle de fluxo.

---
+++ exercicios-variaveis-e-controle-de-fluxo/src/Questao01.java
@@ -0,0 +1,17 @@
+// Escreva um programa para declarar duas variáveis do tipo inteiro e calcular sua soma.
+
+public class Questao01 {
+
+    public static void main(String[] args) {
+
+        int primeiroNumero;
+        int segundoNumero;
+        int soma;
+
+        primeiroNumero = 1;
+        segundoNumero = 2;
+        soma = primeiroNumero + segundoNumero;
+
+        System.out.println(soma);
+    }
+}
