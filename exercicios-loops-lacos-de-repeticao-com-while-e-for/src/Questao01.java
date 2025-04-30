8d76f16b11554f44692456f5ea119dcd94f0c3d4
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-04-30 13:50:33 -0300

    Questão 1 dos exercicios de loops

---
+++ exercicios-loops-lacos-de-repeticao-com-while-e-for/src/Questao01.java
@@ -0,0 +1,14 @@
+public class Questao01 {
+
+    public static void main(String[] args) {
+
+        int valor;
+
+        valor = 0;
+
+        while (valor < 10) {
+            valor++;
+            System.out.println("Valor: " + valor);
+        }
+    }
+}
