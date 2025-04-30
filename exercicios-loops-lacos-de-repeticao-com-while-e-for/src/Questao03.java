8e47a7cb518e01b23c3d283ec6da19d8e32081d2
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-04-30 13:58:12 -0300

    Questão 3 dos exercicios de loops

---
+++ exercicios-loops-lacos-de-repeticao-com-while-e-for/src/Questao03.java
@@ -0,0 +1,13 @@
+// Escreva um programa Java que imprima os números pares de 2 a 20 usando um loop for.
+
+public class Questao03 {
+
+    public static void main(String[] args) {
+
+        int numero;
+
+        for (numero = 0; numero <= 20; numero = numero + 2) {
+            System.out.println(numero);
+        }
+    }
+}
